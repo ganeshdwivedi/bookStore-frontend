2ba294f708c3e1c779801561d22aa2e9610a822e
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2024-01-18 18:51:46 +0530

    Update Books.jsx

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -19,7 +19,7 @@ const Books = () => {
 
 
     return (
-        <div className="grid md:w-[80vw] w-[100vw] grid-cols-2 md:grid-cols-3 md:gap-x-10">
+        <div className="grid pt-[100px] md:w-[80vw] w-[100vw] grid-cols-2 md:grid-cols-3 md:gap-x-10">
             {
                 books.length != 0 ? books.map((item) => <Link key={item._id} to={`/books/${item._id}`}><BookCard title={item.title} type={item.type} price={item.price} author={item.author} imgsrc={item.thumbnail} /></Link>) : <h3>There is no product in library</h3>
             }
