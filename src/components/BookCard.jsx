073782a07b76c53d2da8289f224ab38d75cb3881
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2024-01-19 08:47:29 +0530

    Update BookCard.jsx

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -5,16 +5,14 @@ const BookCard = ({ imgsrc, title, author, type, price }) => {
 
     return (
 
-        <div className='p-3'>
-            <div className='w-[45vw] md:w-[23vw] h-[210px] md:h-[300px] overflow-hidden'><img className='object-cover' src={imgsrc} alt="okay" /></div>
+       <div className='p-3'>
+            <div className='w-[45vw] md:w-[23vw] h-[210px] md:h-[390px]  overflow-hidden'><img className='object-cover' src={imgsrc} alt="okay" /></div>
             <div>
-                <h3 className='text-base font-medium my-1'>{title}</h3>
+                <h3 className='tex-[18px] lg:text-[22px] font-medium my-1'>{title}</h3>
                 <p className='text-slate-700 text-sm my-1'>{author}</p>
-                <p className='text-slate-700 text-sm my-1'>{type}</p>
                 <h3 className='text-[15px] font-medium my-2'>MRP: ₹ {price}</h3>
             </div>
         </div>
-
     )
 }
 
