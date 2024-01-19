28f8de423d68bfd39184e570a869246e1bbaa806
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2024-01-19 09:10:26 +0530

    Update Books.jsx

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -3,7 +3,6 @@ import BookCard from '../components/BookCard'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 
-
 const Books = () => {
     const [books, setBooks] = useState([]);
     const getAllBooks = async () => {
@@ -13,16 +12,13 @@ const Books = () => {
     useEffect(() => {
         getAllBooks()
     }, [])
-
-
+    
     return (
-      <div className="grid pt-[100px] md:w-[80vw] w-[100vw] grid-cols-2 md:grid-cols-3 md:gap-x-10 px-[25px] xl:px-[65px]">
+        <div className="grid pt-[100px] md:w-[80vw] w-[100vw] grid-cols-2 md:grid-cols-3 md:gap-x-10 px-[25px] xl:px-[65px]">
             {
                 books.length != 0 ? books.map((item) => <Link key={item._id} to={`/books/${item._id}`}><BookCard title={item.title} price={item.price} author={item.author} imgsrc={item.thumbnail} /></Link>) : <h3>There is no product in library</h3>
             }
         </div>
     )
-    )
 }
-
 export default Books
