60e5f9c5e884ddc502245835e19c7bd238dd91de
Ganesh Dwivedi <90120686+ganeshdwivedi@users.noreply.github.com>
2024-01-19 08:55:07 +0530

    Update BookDelete.jsx

--- src/components/BookDelete.jsx
+++ src/components/BookDelete.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import axios from 'axios'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const BookDelete = () => {
     const ID = useParams()
-
+    const navigate = useNavigate()
     const token = localStorage.getItem('token')
     const axiosInstance = axios.create({
         baseURL: 'https://book-store-backend-ru34.onrender.com/api',
@@ -17,13 +17,13 @@ const BookDelete = () => {
 
     const Delete = async () => {
         const response = await axiosInstance.delete(`/books/delete/${ID.id}`);
-        console.log(response)
         alert(response.data.message)
+        navigate('/books')
     }
 
 
     return (
-       <div className='py-[100px] flex flex-col items-center px-[25px]'>
+        <div className='py-[100px] flex flex-col items-center px-[25px]'>
             <h3 className='text-[24px] text-center lg:text-[30px] font-[600]'>Do you really want to delete this book</h3>
             <button onClick={Delete} className='bg-red-400 mt-5 hover:bg-white hover:text-red-400 transition-all rounded-md hover:border-red-400 border  text-white px-[35px] py-[10px]'>Delete</button>
         </div>
